89758f308e771de8d54d9c2803da013061606d03
Joseph DelCioppio <joseph.delcioppio@gmail.com>
2014-06-01 11:35:29 -0400

    Add Customer#deposit!
    This is a convience method for increasing the balance of a Customer
    while handling error cases as well

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,21 @@
 class Customer < ActiveRecord::Base
 
+  def deposit!(amount)
+    if amount > 0
+     update(balance: balance + amount)
+    else
+      raise DepositMustBeGreaterThanZero.new(amount)
+    end
+  end
+
+  class DepositMustBeGreaterThanZero < StandardError
+    def initialize(amount)
+      @amount = amount
+    end
+
+    def to_s
+      "Deposit amount must be greater than 0, in this case it was actually #{@amount}"
+    end
+  end
 end
+
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,5 +5,27 @@ describe Customer do
   it 'is valid with valid attributes' do
     expect(Customer.new).to be_valid
   end
+
+  describe '#deposit' do
+    let(:customer) {Customer.create balance: 10}
+
+    context 'if the amount is more than zero' do
+      let(:deposit_amount) { 15 }
+
+      it "increases the customer's balance by the specified amount" do
+        customer.deposit! deposit_amount
+
+        expect(customer.balance).to eql 25
+      end
+    end
+
+    context 'if the amount is less than zero' do
+      let(:deposit_amount) { -1 }
+
+      it 'raises an Customer::DepositMustBeGreatThanZero exception' do
+        expect{customer.deposit!(deposit_amount)}.to raise_error Customer::DepositMustBeGreaterThanZero
+      end
+    end
+  end
 end
 
