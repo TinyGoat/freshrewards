a90f742ccd2a44c019b9a69037fe0534fa6bf7ec
Nick Jones <nick@tinygoatstudio.com>
2014-07-03 13:33:18 -0400

    - Adds updates copy and images per June 26th BC message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,15 @@
 class HomeController < ApplicationController
+  def new
+    @inquiry = Inquiry.new
+  end
+
+  def create
+    @inquiry = Inquiry.new(params[:inquiry])
+
+    if @inquiry.deliver
+      render :create
+    else
+      render :new
+    end
+  end
 end
